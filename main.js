88d838b080fff05f62a7482df8ff6e646cc497fd
malkayman <120787226+malkayman@users.noreply.github.com>
2024-09-11 14:45:39 +0300

    readme

---
+++ main.js
@@ -0,0 +1,40 @@
+
+/*document.write("<h1>heelllo</h>");
+console.log("jjjjj %cmm","color:red");
+console.log(typeof{name:'malak',age:20});
+var x = 'malak';
+console.log(x)
+console.log('hello\'malak\'');
+var a="malaak";
+var b="lovve";
+var c="you";
+console.log(a+""+b+"\n"+c);
+console.log(`${a} ${b} ${c}`);
+let mark=`
+<div class "card">
+    <div class "child>
+    <h2>title</h2>
+    <p>paragraph</p>
+    </div>
+    </div>`;
+    document.write(mark);
+    let a="90";
+    let b=70;
+    let c=true;
+    console.log(+a+b+c);
+    console.log(Number.MAX_VALUE);
+    console.log(parseInt("100 malak"));
+    console.log(("100").toString());
+    console.log(Math.ceil(99.8));*/
+    let m ="malak ayman";
+    console.log(m[1]);
+    console.log(m.trim());
+    console.log(m.trim().charAt(2).toUpperCase());
+    console.log(m.indexOf("ayman"));
+    console.log(m.lastIndexOf("a"));
+    console.log(m.slice(-5));
+    console.log(m.repeat(5));
+    console.log(m.split("a",5));
+    console.log(m.substr(0,7));
+    console.log(m.includes("enab"))
+    console.log(m.endsWith("a",4))
